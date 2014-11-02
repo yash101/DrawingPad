a5b2a9260aa017ea80dfe02ffd7cf79f72b8962b
DevYash <student@Teachers-MacBook-Air-134.local>
2014-11-02 12:33:42 -0700

    save

--- DrawingPad/include/definitions.hxx
+++ DrawingPad/include/definitions.hxx
@@ -3,7 +3,7 @@
 #define DEBUG               false       //Whether to enable the rather long debug messages!
 #define MUTEX_TIMEOUT       100         //Hmmmm... Stuck threads bothering you? Set this timeout wisely and then
                                         //the mutex will expire after X number of milliseconds :)
-#define DISABLE_FILECACHE               //Let's disable the cache. Only for development purposes. Will impact performance.
+//#define DISABLE_FILECACHE               //Let's disable the cache. Only for development purposes. Will impact performance.
                                         //Comment out DISABLE_FILECACHE to disable!
 #define SESSION_TIMEOUT     10000       //How long a session should last before deletion
 #define PORTNO_PAINTSERVER  1234        //What port to use for the main server
