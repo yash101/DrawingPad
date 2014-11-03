879a89781a3135d252efb0280f592401e3578e88
Devyash Lodha <dlodha@pvlearners.net>
2014-11-02 19:16:36 -0700

    save

--- DrawingPad/source/Session.cxx
+++ DrawingPad/source/Session.cxx
@@ -127,81 +127,18 @@ void SessionHost::cron()
                     std::cout << "Kept back session handle: [" << curkey << "]" << std::endl;
                 }
             }
+        }
 
-            for(unsigned int i = 0; i < del.size(); i++)
-            {
-                timestamp.erase(del[i]);
-                data.erase(del[i]);
-                std::cout << "Erasing: " << del[i] << std::endl;
-            }
+        for(unsigned int i = 0; i < del.size(); i++)
+        {
+            timestamp.erase(del[i]);
+            data.erase(del[i]);
+            std::cout << "Erasing: " << del[i] << std::endl;
         }
 
         locky_thingy.unlock();
     }
 }
-
-/*void SessionHost::cron()
-{
-    while(true)
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(20));
-        if(!DEBUG)
-        {
-            std::cout << "Cron Running!" << std::endl;
-        }
-<<<<<<< HEAD
-        while(!locky_thingy.try_lock_for(std::chrono::milliseconds(MUTEX_TIMEOUT))) {}
-        for(auto i = timestamp.begin(); i != timestamp.end(); ++i)
-=======
-        while(!locky_thingy.try_lock_for(std::chrono::milliseconds(MUTEX_TIMEOUT)));
-        for(auto i = data.begin(); i != data.end(); ++i)
->>>>>>> bb58229f04b789896727f168c6e89866131fdbb1
-        {
-            if(DEBUG)
-            {
-                std::cout << "Checking " << i->first << " with timestamp: " << timestamp[i->first] << std::endl;
-            }
-            unsigned long x = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch()).count();
-            if(DEBUG)
-            {
-                std::cout << "Current time: " << x << std::endl;
-            }
-            unsigned long tdiff = x - timestamp[i->first];
-            if(tdiff > SESSION_TIMEOUT)
-            {
-                if(data.find(i->first) != data.end())
-                {
-                    data.erase(i->first);
-                }
-                else
-                {
-                    std::cout << i->first << " Was not found in data!" << std::endl;
-                }
-                if(timestamp.find(i->first) != timestamp.end())
-                {
-                    timestamp.erase(i->first);
-                }
-                else
-                {
-                    std::cout << i->first << " Was not found in timestamp!" << std::endl;
-                }
-
-                if(DEBUG)
-                {
-                    std::cout << "Killed Session [" << i->first << "]!" << std::endl;
-                }
-            }
-            else
-            {
-                if(DEBUG)
-                {
-                    std::cout << "Skipped Session [" << i->first << "]!" << std::endl;
-                }
-            }
-        }
-        locky_thingy.unlock();
-    }
-}*/
 
 void SessionHost::keepAlive(std::string sess)
 {
